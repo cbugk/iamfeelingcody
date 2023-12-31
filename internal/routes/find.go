7ccef014f117533f4980284486bc46353ff947f9
Celil Buğra Karacan <49951195+cbugk@users.noreply.github.com>
2023-12-31 16:25:02 +0300

    Move logic out of routes package into misc

--- internal/routes/find.go
+++ internal/routes/find.go
@@ -1,32 +1,12 @@
 package routes
 
 import (
-	"fmt"
 	"net/http"
 
+	"github.com/cbugk/iamfeelingcody/internal/misc"
 	"github.com/cbugk/iamfeelingcody/internal/templates"
-	"github.com/dchest/uniuri"
 )
 
 func find(w http.ResponseWriter, r *http.Request) {
-	templates.PageFind(findGithubUser(1)).Render(r.Context(), w)
-}
-
-func findGithubUser(length int) string {
-	userURI := ""
-
-	for true {
-		userURI = fmt.Sprintf("https://github.com/%s", uniuri.NewLen(length))
-
-		if resp, err := http.Get(userURI); err != nil {
-			fmt.Println(err)
-		} else if resp.StatusCode != 200 {
-			fmt.Println("Non-OK status code: %s, %s", resp.StatusCode, userURI)
-		} else {
-			fmt.Printf("Found: %s\n", userURI)
-			break
-		}
-	}
-
-	return userURI
+	templates.PageFind(misc.FindGithubUser(1)).Render(r.Context(), w)
 }
