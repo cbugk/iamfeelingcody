c6567d1495c51e1ebbeafdada96577e4244f26c1
Celil Buğra Karacan <49951195+cbugk@users.noreply.github.com>
2024-01-09 22:20:51 +0300

    Renamed templates package as templ

--- internal/routes/find.go
+++ internal/routes/find.go
@@ -6,23 +6,23 @@ import (
 	"net/http"
 
 	"github.com/cbugk/iamfeelingcody/internal/model"
-	"github.com/cbugk/iamfeelingcody/internal/templates"
+	"github.com/cbugk/iamfeelingcody/internal/templ"
 	"github.com/julienschmidt/httprouter"
 )
 
 func find(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	// Empty name
 	if name := r.URL.Query().Get("name"); name == "" {
-		templates.PageFindErrProvideName().Render(r.Context(), w)
+		templ.PageFindErrProvideName().Render(r.Context(), w)
 	} else {
 		user := model.MakeGithubUser(name)
 
 		err := model.CheckGithubUser(user)
 
 		if err == nil {
-			templates.PageGithubUserFound(user).Render(r.Context(), w)
+			templ.PageGithubUserFound(user).Render(r.Context(), w)
 		} else if errors.Is(err, &model.ErrorGithubUserNotFound{}) {
-			templates.PageGithubUserNotFound(user).Render(r.Context(), w)
+			templ.PageGithubUserNotFound(user).Render(r.Context(), w)
 		} else {
 			log.Fatal(err.Error())
 		}
--- internal/routes/users.go
+++ internal/routes/users.go
@@ -2,11 +2,12 @@ package routes
 
 import (
 	"net/http"
+
 	"github.com/cbugk/iamfeelingcody/internal/db"
-	"github.com/cbugk/iamfeelingcody/internal/templates"
+	"github.com/cbugk/iamfeelingcody/internal/templ"
 	"github.com/julienschmidt/httprouter"
 )
 
 func users(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	templates.PageFoundUsers(db.AllGithubUsers()).Render(r.Context(), w)
+	templ.PageFoundUsers(db.AllGithubUsers()).Render(r.Context(), w)
 }
