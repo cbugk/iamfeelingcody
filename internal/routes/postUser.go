4b818476136a3476d458ba897c503c09f58ff503
Celil Buğra Karacan <49951195+cbugk@users.noreply.github.com>
2024-01-31 00:01:42 +0300

    Fixed recreation of existing user POST /user

--- internal/routes/postUser.go
+++ internal/routes/postUser.go
@@ -20,15 +20,20 @@ func postUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	// name not provided
 	if len(name) == 0 {
 		w.WriteHeader(http.StatusNoContent)
-	} else if err := check.CheckGithubUser(name); err == nil {
-		w.WriteHeader(http.StatusCreated)
-		_, err = sqlc.Q().CreateGithubUser(ctx, name)
-		if err != nil {
+	} else if _, err := sqlc.Q().GetGithubUser(ctx, name); err != nil {
+		if err := check.CheckGithubUser(name); err == nil {
+
+			w.WriteHeader(http.StatusCreated)
+			_, err = sqlc.Q().CreateGithubUser(ctx, name)
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+		} else if errors.Is(err, &check.ErrorGithubUserNotFound{}) {
+			w.WriteHeader(http.StatusNoContent)
+		} else {
 			log.Fatal(err.Error())
 		}
-	} else if errors.Is(err, &check.ErrorGithubUserNotFound{}) {
-		w.WriteHeader(http.StatusNoContent)
 	} else {
-		log.Fatal(err.Error())
+		w.WriteHeader(http.StatusFound)
 	}
 }
